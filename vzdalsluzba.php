703541641ccccfcef01bda517df03debf5e199e8
Zbynek Dujsik <zbynek.dujsik1@seznam.cz>
2020-04-09 15:32:30 +0200

    Initial commit

---
+++ vzdalsluzba.php
@@ -0,0 +1,10 @@
+
+
+<?php
+header("Content-Type: application/json");
+
+$data = array();
+$data["jmeno"] = "Libor";
+$data["prijmeni"] = "Svejda";
+$data["roknar"] = 2000;
+echo json_encode($data);
